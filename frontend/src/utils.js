d9da11593278881b376ebd057b548669594a7e6e
fathhaq12 <fathahillahhaq@gmail.com>
2025-05-26 21:43:54 +0700

    up

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,1 @@
 export const BASE_URL = "https://be-review-restoran-1061342868557.us-central1.run.app/";
-<<<<<<< HEAD
-
-=======
-export const API_URL = "${BASE_URL}/api/"; 
->>>>>>> 23f8665d17b35b525f4eac60c24cc639de6fd1bd
