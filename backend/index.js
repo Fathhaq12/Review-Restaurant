60fef37a04d6980f712e9bd2c92f629d31c0bcf8
fathhaq12 <fathahillahhaq@gmail.com>
2025-05-27 13:58:51 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ import "./model/ReviewModel.js";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
