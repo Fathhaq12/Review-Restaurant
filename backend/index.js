c58277ba7360ab3fc44dc5bb7a1492f52d9e6469
fathhaq12 <fathahillahhaq@gmail.com>
2025-05-27 13:36:30 +0700

    bismillah

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ import "./model/ReviewModel.js";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(
